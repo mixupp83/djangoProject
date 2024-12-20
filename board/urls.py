33eb911e3e5d15275c87389d6fe4e5d85a09d452
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-12-20 10:55:03 +0400

    Проект: Доска объявлений на Django

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
     path('advertisement/<int:pk>/', views.advertisement_detail, name='advertisement_detail'),
     path('add/', views.add_advertisement, name='add_advertisement'),
+    path('advertisement/<int:pk>/edit/', views.edit_advertisement, name='edit_advertisement'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, redirect
-from board.models import Advertisement
-from board.forms import AdvertisementForm, SignUpForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Advertisement
+from .forms import AdvertisementForm, SignUpForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login, authenticate
 
+# Представление для выхода
 def logout_view(request):
     logout(request)
     return redirect('home')
 
+# Представление для регистрации
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -19,17 +21,21 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'board/signup.html', {'form': form})
 
+# Представление для домашней страницы
 def home(request):
     return render(request, 'home.html')
 
+# Представление для списка объявлений
 def advertisement_list(request):
     advertisements = Advertisement.objects.all()
     return render(request, 'board/advertisement_list.html', {'advertisements': advertisements})
 
+# Представление для деталей объявления
 def advertisement_detail(request, pk):
     advertisement = Advertisement.objects.get(pk=pk)
     return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
 
+# Представление для добавления объявления
 @login_required
 def add_advertisement(request):
     if request.method == "POST":
@@ -42,3 +48,21 @@ def add_advertisement(request):
     else:
         form = AdvertisementForm()
     return render(request, 'board/add_advertisement.html', {'form': form})
+
+
+@login_required
+def edit_advertisement(request, pk):
+    advertisement = get_object_or_404(Advertisement, pk=pk)
+
+    if advertisement.author != request.user:
+        return redirect('board:advertisement_list')
+
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, instance=advertisement)
+        if form.is_valid():
+            form.save()
+            return redirect('board:advertisement_detail', pk=advertisement.pk)
+    else:
+        form = AdvertisementForm(instance=advertisement)
+
+    return render(request, 'board/edit_advertisement.html', {'form': form, 'advertisement': advertisement})
